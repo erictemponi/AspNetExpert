ad6640f1c0e0e501d90356074ba64866395de48f
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-24 10:19:08 -0300

    Adicionar Swagger na Startup

--- src/services/Gouro.Identidade.API/Startup.cs
+++ src/services/Gouro.Identidade.API/Startup.cs
@@ -1,18 +1,13 @@
 using Gouro.Identidade.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Gouro.Identidade.API
 {
@@ -37,11 +32,28 @@ namespace Gouro.Identidade.API
                 .AddDefaultTokenProviders();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Grupo Grão de Ouro - API de Identidade",
+                    Description = "API de Identidade",
+                    Contact = new OpenApiContact() { Name = "Eric Temponi", Email = "eric.temponi@gouro.com.br" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
