31bce2cd9d7fb338c85b0c1813ee604cac00a6cd
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-25 10:05:38 -0300

    Model, View e Controller de login e registro

---
+++ src/web/Gouro.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,59 @@
+﻿using Gouro.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gouro.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            // Registro na API
+            if (false) return View(usuarioRegistro);
+
+            // Realizar login na aplicação
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // Login na API
+            if (false) return View(usuarioLogin);
+
+            // Realizar login na aplicação
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
