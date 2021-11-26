414518a5a911b1e4298994991c032534529ba3ce
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-26 12:03:39 -0300

    Pequenas refatorações

--- src/web/Gouro.WebApp.MVC/Services/Service.cs
+++ src/web/Gouro.WebApp.MVC/Services/Service.cs
@@ -28,13 +28,14 @@ namespace Gouro.WebApp.MVC.Services
 
         protected bool TratarErrosResponse(HttpResponseMessage response)
         {
-            switch((int)response.StatusCode)
+            switch ((int)response.StatusCode)
             {
                 case 401:
                 case 403:
                 case 404:
                 case 500:
                     throw new CustomHttpRequestException(response.StatusCode);
+
                 case 400:
                     return false;
             }
