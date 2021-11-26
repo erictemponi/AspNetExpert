42c40f56010fbabcf29ad7e89fc59d6aa0e757a6
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-26 12:01:41 -0300

    Mudar redirecionamento da página de acesso negado

--- src/web/Gouro.WebApp.MVC/Configuration/IdentityConfig.cs
+++ src/web/Gouro.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -11,7 +11,7 @@ namespace Gouro.WebApp.MVC.Configuration
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
             {
                 options.LoginPath = "/login";
-                options.AccessDeniedPath = "/acesso-negado";
+                options.AccessDeniedPath = "/erro/403";
             });
         }
 
