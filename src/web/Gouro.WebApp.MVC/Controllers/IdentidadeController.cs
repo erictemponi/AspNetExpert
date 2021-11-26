64cb3b5765683e0e0bd8c199648dc29dba135f52
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-26 10:50:10 -0300

    Pegar a URL onde o usuário estava antes do login para retorná-lo a ela

--- src/web/Gouro.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/Gouro.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -46,15 +46,18 @@ namespace Gouro.WebApp.MVC.Controllers
 
         [HttpGet]
         [Route("login")]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin, string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
+
             if (!ModelState.IsValid) return View(usuarioLogin);
 
             // Login na API
@@ -65,7 +68,9 @@ namespace Gouro.WebApp.MVC.Controllers
             // Realizar login na aplicação
             await RealizarLogin(resposta);
 
-            return RedirectToAction("Index", "Home");
+            if (string.IsNullOrEmpty(returnUrl)) return RedirectToAction("Index", "Home");
+
+            return LocalRedirect(returnUrl);
         }
 
         [HttpGet]
