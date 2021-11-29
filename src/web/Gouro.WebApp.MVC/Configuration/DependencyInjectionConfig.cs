dede5c0e424e63e84da393cbf2ddce895e9f944a
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-29 13:34:49 -0300

    Injestão de dependência do catálogo

--- src/web/Gouro.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/Gouro.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -10,6 +10,7 @@ namespace Gouro.WebApp.MVC.Configuration
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
