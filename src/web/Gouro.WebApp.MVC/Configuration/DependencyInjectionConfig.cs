45fa2de8fce04ccc03c09119851137dba3c9873c
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-30 10:34:50 -0300

    Comentar o uso do Refit

--- src/web/Gouro.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/Gouro.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -16,15 +16,15 @@ namespace Gouro.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            //services.AddHttpClient<ICatalogoService, CatalogoService>()
-            //        .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
-            services.AddHttpClient("Refit", options =>
-            {
-                options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
-            })
-                    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-                    .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+            //services.AddHttpClient("Refit", options =>
+            //{
+            //    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+            //})
+            //        .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //        .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
