6775ed771b741cfc69b77f5eebecd98281e84c82
Erictemponi <erictemponi_vdl@hotmail.com>
2021-11-30 09:52:29 -0300

    Remover referências não utilizadas da Controller Home

--- src/web/Gouro.WebApp.MVC/Controllers/HomeController.cs
+++ src/web/Gouro.WebApp.MVC/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
 ﻿using Gouro.WebApp.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Gouro.WebApp.MVC.Controllers
 {
