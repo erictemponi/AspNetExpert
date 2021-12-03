36b445b163c05d2b1531dc92f87b69b38fb0dfda
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-03 12:02:18 -0300

    Funções para manipular o carrinho

--- src/services/Gouro.Carrinho.API/Models/CarrinhoItem.cs
+++ src/services/Gouro.Carrinho.API/Models/CarrinhoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 
 namespace Gouro.Carrinho.API.Models
 {
@@ -17,6 +18,52 @@ namespace Gouro.Carrinho.API.Models
         public CarrinhoItem()
         {
             Id = Guid.NewGuid();
+        }
+
+        internal void AssociarCarrinho(Guid carrinhoId)
+        {
+            CarrinhoId = carrinhoId;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * Valor;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal bool EhValido()
+        {
+            return new ItemPedidoValidation().Validate(this).IsValid;
+        }
+    }
+
+    public class ItemPedidoValidation : AbstractValidator<CarrinhoItem>
+    {
+        public ItemPedidoValidation()
+        {
+            RuleFor(c => c.ProdutoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do produto inválido");
+
+            RuleFor(c => c.Nome)
+                .NotEmpty()
+                .WithMessage("O nome do produto não foi informado");
+
+            RuleFor(c => c.Quantidade)
+                .GreaterThan(0)
+                .WithMessage("A quantidade mínima de um item é 1");
+
+            RuleFor(c => c.Quantidade)
+                .LessThan(CarrinhoCliente.MAX_QUANTIDADE_ITEM)
+                .WithMessage($"A quantidade máxima de um item é {CarrinhoCliente.MAX_QUANTIDADE_ITEM}");
+
+            RuleFor(c => c.Valor)
+                .GreaterThan(0)
+                .WithMessage("O valor do item precia ser maior que 0");
         }
     }
 }
