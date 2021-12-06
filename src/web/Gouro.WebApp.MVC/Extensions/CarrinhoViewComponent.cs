8feb35e356efa4f349f09d85da93731414d324e1
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-06 10:15:52 -0300

    Extensões do Carrinho no MVC

---
+++ src/web/Gouro.WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Gouro.WebApp.MVC.Models;
+using Gouro.WebApp.MVC.Services;
+
+namespace Gouro.WebApp.MVC.Extensions
+{
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        private readonly ICarrinhoService _carrinhoService;
+
+        public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+        {
+            _carrinhoService = carrinhoService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+        }
+    }
+}
