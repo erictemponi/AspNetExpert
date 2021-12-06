052459176d192579710a3ab268871557391f9555
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-06 12:04:45 -0300

    Liberar a API catálogo para qualquer usuário

--- src/services/Gouro.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/Gouro.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,6 +1,5 @@
 ﻿using Gouro.Catalogo.API.Models;
 using Gouro.WebApi.Core.Controllers;
-using Gouro.WebApi.Core.Identidade;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,7 +8,6 @@ using System.Threading.Tasks;
 
 namespace Gouro.Catalogo.API.Controllers
 {
-    [Authorize]
     public class CatalogoController : MainController
     {
         private readonly IProdutoRepository _produtoRepository;
@@ -19,14 +17,12 @@ namespace Gouro.Catalogo.API.Controllers
             _produtoRepository = produtoRepository;
         }
 
-        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
             return await _produtoRepository.ObterTodos();
         }
 
-        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> ProdutoDetalhe(Guid id)
         {
