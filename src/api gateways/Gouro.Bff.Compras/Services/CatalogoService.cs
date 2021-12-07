03ccd288f0bf9c6632a1ad9fed9a825b3dab4af4
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-07 10:42:42 -0300

    Obter produto por ID no Serviço do catálogo

--- src/api gateways/Gouro.Bff.Compras/Services/CatalogoService.cs
+++ src/api gateways/Gouro.Bff.Compras/Services/CatalogoService.cs
@@ -1,12 +1,15 @@
 ﻿using Gouro.Bff.Compras.Extensions;
+using Gouro.Bff.Compras.Models;
 using Microsoft.Extensions.Options;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Gouro.Bff.Compras.Services
 {
     public interface ICatalogoService
     {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
     }
 
     public class CatalogoService : Service, ICatalogoService
@@ -17,6 +20,15 @@ namespace Gouro.Bff.Compras.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+        }
+
+        public async Task<ItemProdutoDTO?> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DesserializarObjetoResponse<ItemProdutoDTO>(response);
         }
     }
 }
