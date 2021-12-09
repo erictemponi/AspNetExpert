d870f8f3e5e72366006f2d8f06f4d11f6ebc0d4b
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-09 08:31:46 -0300

    Controllers Pedido e Voucher iniciais

---
+++ src/services/Gouro.Pedido.API/Controllers/PedidoController.cs
@@ -0,0 +1,20 @@
+﻿using Gouro.Core.Mediator;
+using Gouro.WebApi.Core.Controllers;
+using Gouro.WebApi.Core.Usuario;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Gouro.Pedidos.API.Controllers
+{
+    [Authorize]
+    public class PedidoController : MainController
+    {
+        private readonly IMediatorHandler _mediator;
+        private readonly IAspNetUser _user;
+
+        public PedidoController(IMediatorHandler mediator, IAspNetUser user)
+        {
+            _mediator = mediator;
+            _user = user;
+        }
+    }
+}
--- src/services/Gouro.Pedido.API/Controllers/VoucherController.cs
+++ src/services/Gouro.Pedido.API/Controllers/VoucherController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Gouro.WebApi.Core.Controllers;
+using Gouro.WebApi.Core.Usuario;
+using Gouro.Pedidos.Infra.Data;
 
 namespace Gouro.Pedidos.API.Controllers
 {
     [Authorize]
     public class VoucherController : MainController
     {
+        private readonly IAspNetUser _user;
+        private readonly PedidosContext _context;
 
+        public VoucherController(IAspNetUser user, PedidosContext context)
+        {
+            _user = user;
+            _context = context;
+        }
     }
 }
