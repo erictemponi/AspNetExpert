426282db21523727820e65b956d6bfebed86507b
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-09 09:48:18 -0300

    DTO, Queries e Especificações do Voucher

--- src/services/Gouro.Pedidos.Domain/Vouchers/Voucher.cs
+++ src/services/Gouro.Pedidos.Domain/Vouchers/Voucher.cs
@@ -1,4 +1,5 @@
 ﻿using Gouro.Core.DomainObjects;
+using Gouro.Pedidos.Domain.Vouchers.Specs;
 using System;
 
 namespace Gouro.Pedidos.Domain.Vouchers
@@ -15,5 +16,19 @@ namespace Gouro.Pedidos.Domain.Vouchers
         public DateTime DataValidade { get; private set; }
         public bool Ativo { get; private set; }
         public bool Utilizado { get; private set; }
+
+        public bool EstaValidoParaUtilizacao()
+        {
+            return new VoucherAtivoSpecification()
+                .And(new VoucherDataSpecification())
+                .And(new VoucherQuantidadeSpecification()).IsSatisfiedBy(this);
+        }
+
+        public void MarcarComoUtilizado()
+        {
+            Ativo = false;
+            Utilizado = true;
+            Quantidade = 0;
+        }
     }
 }
