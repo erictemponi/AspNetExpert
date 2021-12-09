6dbf820d7d62703de9cefa3e384a7e5f9e3c5d14
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-09 11:39:32 -0300

    Aplicar Voucher

--- src/web/Gouro.WebApp.MVC/Services/ComprasBffService.cs
+++ src/web/Gouro.WebApp.MVC/Services/ComprasBffService.cs
@@ -15,6 +15,7 @@ namespace Gouro.WebApp.MVC.Services
         Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoViewModel produto);
         Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel produto);
         Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
+        Task<ResponseResult> AplicarVoucherCarrinho(string voucher);
     }
 
     public class ComprasBffService : Service, IComprasBffService
@@ -66,6 +67,17 @@ namespace Gouro.WebApp.MVC.Services
         public async Task<ResponseResult> RemoverItemCarrinho(Guid produtoId)
         {
             var response = await _httpClient.DeleteAsync($"/compras/carrinho/items/{produtoId}");
+
+            if (!TratarErrosResponse(response)) return await DesserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<ResponseResult> AplicarVoucherCarrinho(string voucher)
+        {
+            var itemContent = ObterConteudo(voucher);
+
+            var response = await _httpClient.PostAsync("compras/carrinho/aplicar-voucher/", itemContent);
 
             if (!TratarErrosResponse(response)) return await DesserializarObjetoResponse<ResponseResult>(response);
 
