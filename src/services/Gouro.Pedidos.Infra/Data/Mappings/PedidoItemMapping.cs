acb173a2b8656078a69fb75d8060071ce3918bef
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-10 11:23:59 -0300

    Modelagem, repositório e mapeamento dos Pedidos

---
+++ src/services/Gouro.Pedidos.Infra/Data/Mappings/PedidoItemMapping.cs
@@ -0,0 +1,24 @@
+﻿using Gouro.Pedidos.Domain.Pedidos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gouro.Pedidos.Infra.Data.Mappings
+{
+    public class PedidoItemMapping : IEntityTypeConfiguration<PedidoItem>
+    {
+        public void Configure(EntityTypeBuilder<PedidoItem> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.ProdutoNome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            // 1 : N => Pedido : Pagamento
+            builder.HasOne(c => c.Pedido)
+                .WithMany(c => c.PedidoItens);
+
+            builder.ToTable("PedidoItens");
+        }
+    }
+}
