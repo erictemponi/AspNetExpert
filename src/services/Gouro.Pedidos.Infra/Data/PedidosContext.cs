9a9ef6de33a7feda038ee787f3b2327ec301794c
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-10 11:24:12 -0300

    Contexto e Migrations dos pedidos

--- src/services/Gouro.Pedidos.Infra/Data/PedidosContext.cs
+++ src/services/Gouro.Pedidos.Infra/Data/PedidosContext.cs
@@ -6,6 +6,7 @@ using Gouro.Core.Data;
 using Gouro.Core.DomainObjects;
 using Gouro.Core.Mediator;
 using Gouro.Core.Messages;
+using Gouro.Pedidos.Domain.Pedidos;
 using Gouro.Pedidos.Domain.Vouchers;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,7 +22,8 @@ namespace Gouro.Pedidos.Infra.Data
             _mediatorHandler = mediatorHandler;
         }
 
-
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoItem> PedidoItens { get; set; }
         public DbSet<Voucher> Vouchers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -34,10 +36,29 @@ namespace Gouro.Pedidos.Infra.Data
             modelBuilder.Ignore<ValidationResult>();
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(PedidosContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.HasSequence<int>("MinhaSequencia").StartsAt(1000).IncrementsBy(1);
+            base.OnModelCreating(modelBuilder);
         }
 
         public async Task<bool> Commit()
         {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
             var sucesso = await base.SaveChangesAsync() > 0;
             if (sucesso) await _mediatorHandler.PublicarEventos(this);
 
