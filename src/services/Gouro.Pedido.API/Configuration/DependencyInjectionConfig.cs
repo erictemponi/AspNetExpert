0d838bb9f292add31ec9ce138c87dda922a043d9
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-13 10:47:34 -0300

    Adicionar, último e listar Pedidos

--- src/services/Gouro.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/Gouro.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,14 @@
-﻿using Gouro.Core.Mediator;
+﻿using FluentValidation.Results;
+using Gouro.Core.Mediator;
+using Gouro.Pedidos.API.Application.Commands;
+using Gouro.Pedidos.API.Application.Events;
 using Gouro.Pedidos.API.Application.Queries;
+using Gouro.Pedidos.Domain.Pedidos;
 using Gouro.Pedidos.Domain.Vouchers;
 using Gouro.Pedidos.Infra.Data;
 using Gouro.Pedidos.Infra.Data.Repository;
 using Gouro.WebApi.Core.Usuario;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,11 +22,19 @@ namespace Gouro.Pedidos.API.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
+
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             // Data
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosContext>();
         }
