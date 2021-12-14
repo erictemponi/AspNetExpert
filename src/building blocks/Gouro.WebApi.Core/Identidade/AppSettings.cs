5b11ce1eba3f3bfc111477fcd3371dd1561e9a12
Erictemponi <erictemponi_vdl@hotmail.com>
2021-12-14 11:02:34 -0300

    Referências não utilizadas e espaçamento

--- src/building blocks/Gouro.WebApi.Core/Identidade/AppSettings.cs
+++ src/building blocks/Gouro.WebApi.Core/Identidade/AppSettings.cs
@@ -3,11 +3,8 @@
     public class AppSettings
     {
         public string Secret { get; set; }
-
         public int ExpiracaoHoras { get; set; }
-
         public string Emissor { get; set; }
-
         public string ValidoEm { get; set; }
     }
 }
